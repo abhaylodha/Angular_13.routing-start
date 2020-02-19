780047f52c283b279ae22ac66c9f2be4d8f5dc77
Abhaykumar Lodha <abhaykumar.lodha@gmail.com>
2020-02-19 23:37:53 +0530

    first commit

---
+++ src/app/servers/server/server.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { ServersService } from '../servers.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+
+export class ServerComponent implements OnInit, OnDestroy {
+  server: { id: number, name: string, status: string };
+  paramSubscription: Subscription;
+
+  constructor(private serversService: ServersService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
+
+    this.paramSubscription = this.route.params.subscribe((params: Params) => {
+      this.server = this.serversService.getServer(+params['id']);
+    });
+  }
+
+  ngOnDestroy() {
+    this.paramSubscription.unsubscribe();
+  }
+}
